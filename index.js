03e7987605f8301d7bea72da8e7b2f19da049004
Tatiana <tatianamontoya0428@gmail.com>
2024-10-09 13:19:39 -0500

    cambios

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(urlencoded({extended: true}));
 app.use(json());
 app.use(cors());
 
+app.get('/', async (req, res)=> {
+    res.send("hola soy el backend de la calculadora de Tatiana");
+})
 app.use('/v1/calculadora', router);
 
 app.listen(3500, ()=>{
